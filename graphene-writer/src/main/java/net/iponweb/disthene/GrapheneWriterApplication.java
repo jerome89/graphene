033537afa318434773f85bef3e395fbad353e781
dark <ekdxhrl0096@gmail.com>
2019-09-21 20:23:59 +0900

    feat: move kotlin file to kotlin directory

--- graphene-writer/src/main/java/net/iponweb/disthene/GrapheneWriterApplication.java
+++ graphene-writer/src/main/java/net/iponweb/disthene/GrapheneWriterApplication.java
@@ -1,6 +1,5 @@
 package net.iponweb.disthene;
 
-import com.graphene.writer.GrapheneWriterConfig;
 import net.iponweb.disthene.config.*;
 import net.iponweb.disthene.service.aggregate.AggregationConfiguration;
 import net.iponweb.disthene.service.aggregate.CarbonConfiguration;
@@ -11,10 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"com.graphene.writer", "net.iponweb.disthene"})
-@Import(GrapheneWriterConfig.class)
 @EnableAutoConfiguration(exclude = {CassandraAutoConfiguration.class, GsonAutoConfiguration.class})
 @EnableConfigurationProperties({BlacklistConfiguration.class, DistheneConfiguration.class, CarbonConfiguration.class, AggregationConfiguration.class, IndexConfiguration.class, StatsConfiguration.class, StoreConfiguration.class})
 public class GrapheneWriterApplication {
