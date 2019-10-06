3cb00a11feb5b067cdaaac7a404db92a51dd0d2c
dark <ekdxhrl0096@gmail.com>
2019-10-06 23:26:51 +0900

    test: fix the test code for static factory method

--- graphene-reader/src/test/kotlin/com/graphene/reader/service/index/ElasticsearchIndexServiceTest.kt
+++ graphene-reader/src/test/kotlin/com/graphene/reader/service/index/ElasticsearchIndexServiceTest.kt
@@ -2,8 +2,13 @@ package com.graphene.reader.service.index
 
 import io.mockk.every
 import io.mockk.mockk
+import org.elasticsearch.action.search.SearchResponse
+import org.elasticsearch.search.aggregations.InternalAggregations
+import org.elasticsearch.search.facet.InternalFacets
 import org.elasticsearch.search.internal.InternalSearchHit
 import org.elasticsearch.search.internal.InternalSearchHits
+import org.elasticsearch.search.internal.InternalSearchResponse
+import org.elasticsearch.search.suggest.Suggest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -23,14 +28,16 @@ internal class ElasticsearchIndexServiceTest {
   @Test
   internal fun `should return deduplicated hierarchy metric path`() {
     // given
-    val response = response(arrayOf(
-      internalSearchHit(Pair("path", "hosts.i-a.cpu.usage")),
-      internalSearchHit(Pair("path", "hosts.i-b.cpu.usage")),
-      internalSearchHit(Pair("path", "hosts.i-c.cpu.usage"))
-    ))
+    val response = ElasticsearchClient.Response.of(
+      searchResponse(
+        arrayOf(
+          internalSearchHit(Pair("path", "hosts.i-a.cpu.usage")),
+          internalSearchHit(Pair("path", "hosts.i-b.cpu.usage")),
+          internalSearchHit(Pair("path", "hosts.i-c.cpu.usage"))))
+    )
 
     every { elasticsearchClient.query(any()) } answers { response }
-    every { elasticsearchClient.searchScroll(any()) } answers { response(arrayOf()) }
+    every { elasticsearchClient.searchScroll(any()) } answers { emptyResponse() }
 
     // when
     val hierarchyMetricPath = elasticsearchIndexService.getHierarchyMetricPaths("NONE", "hosts.*.cpu.*")
@@ -39,12 +46,26 @@ internal class ElasticsearchIndexServiceTest {
     assertEquals(1, hierarchyMetricPath.size)
   }
 
-  private fun response(hits: Array<InternalSearchHit>): ElasticsearchClient.Response {
-    return ElasticsearchClient.Response(UUID.randomUUID().toString(), InternalSearchHits(
-      hits,
-      hits.size.toLong(),
-      0.0f
-    ), hits.size)
+  private fun emptyResponse() = ElasticsearchClient.Response.of(searchResponse(emptyArray()))
+
+  private fun searchResponse(internalSearchHits: Array<InternalSearchHit>): SearchResponse {
+    val internalSearchResponse = InternalSearchResponse(
+      InternalSearchHits(internalSearchHits, internalSearchHits.size.toLong(), 0.0f),
+      InternalFacets(emptyList()),
+      InternalAggregations.EMPTY,
+      Suggest(),
+      false,
+      false
+    )
+
+    return SearchResponse(
+      internalSearchResponse,
+      UUID.randomUUID().toString(),
+      10,
+      10,
+      1000,
+      emptyArray()
+    )
   }
 
   private fun internalSearchHit(pathPair: Pair<String, Any>): InternalSearchHit {
