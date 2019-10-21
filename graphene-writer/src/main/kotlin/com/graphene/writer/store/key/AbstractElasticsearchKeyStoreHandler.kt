652f93724f2ebd706355980f4834bf268a7e5ec0
dark <ekdxhrl0096@gmail.com>
2019-10-21 11:09:30 +0900

    refactor: remove unused class file in key store handler

--- graphene-writer/src/main/kotlin/com/graphene/writer/store/key/AbstractElasticsearchKeyStoreHandler.kt
+++ graphene-writer/src/main/kotlin/com/graphene/writer/store/key/AbstractElasticsearchKeyStoreHandler.kt
@@ -4,7 +4,8 @@ import com.graphene.writer.input.GrapheneMetric
 import com.graphene.writer.store.KeyStoreHandler
 import com.graphene.writer.store.key.model.ElasticsearchFactory
 import com.graphene.writer.store.key.model.ElasticsearchKeyStoreHandlerProperty
-import com.graphene.writer.store.key.model.GrapheneKeyMapper
+import com.graphene.writer.store.key.rotator.KeyRotator
+import com.graphene.writer.store.key.rotator.SimpleKeyRotator
 import com.graphene.writer.util.NamedThreadFactory
 import net.iponweb.disthene.reader.utils.DateTimeUtils
 import org.apache.log4j.Logger
@@ -25,17 +26,18 @@ import javax.annotation.PreDestroy
 // TODO duplicated branch node removing logic
 abstract class AbstractElasticsearchKeyStoreHandler(
   private val elasticsearchFactory: ElasticsearchFactory,
-  private var property: ElasticsearchKeyStoreHandlerProperty
+  private val property: ElasticsearchKeyStoreHandlerProperty
 ) : KeyStoreHandler, Runnable {
 
   private val logger = Logger.getLogger(SimpleKeyStoreHandler::class.java)
 
   private lateinit var client: RestHighLevelClient
-  private lateinit var scheduler: ScheduledExecutorService
+  private lateinit var keyRotator: KeyRotator
+  private lateinit var keyStoreScheduler: ScheduledExecutorService
+  private lateinit var keyRotatorScheduler: ScheduledExecutorService
   private lateinit var multiGetRequestContainer: MultiGetRequestContainer
   private lateinit var index: String
   private lateinit var type: String
-  private lateinit var grapheneKeyMapper: GrapheneKeyMapper
 
   private var lastFlushTimeSeconds = DateTimeUtils.currentTimeSeconds()
   private var batchSize: Int = 0
@@ -47,16 +49,18 @@ abstract class AbstractElasticsearchKeyStoreHandler(
   fun init() {
     client = elasticsearchFactory.restHighLevelClient()
 
-    grapheneKeyMapper = GrapheneKeyMapper()
-
     index = property.index
     type = property.type
     multiGetRequestContainer = MultiGetRequestContainer()
     batchSize = property.bulk!!.actions
     flushInterval = property.bulk!!.interval
 
-    scheduler = Executors.newSingleThreadScheduledExecutor(NamedThreadFactory(SimpleKeyStoreHandler::class.simpleName!!))
-    scheduler.scheduleWithFixedDelay(this, 3000, 100, TimeUnit.MILLISECONDS)
+    keyStoreScheduler = Executors.newSingleThreadScheduledExecutor(NamedThreadFactory(SimpleKeyStoreHandler::class.simpleName!!))
+    keyStoreScheduler.scheduleWithFixedDelay(this, 3_000, 500, TimeUnit.MILLISECONDS)
+
+    keyRotator = SimpleKeyRotator(property, elasticsearchFactory)
+    keyRotatorScheduler = Executors.newSingleThreadScheduledExecutor(NamedThreadFactory(KeyRotator::class.simpleName!!))
+    keyRotatorScheduler.scheduleWithFixedDelay(keyRotator, 3_000, 60_000, TimeUnit.MILLISECONDS)
 
     client.indices().putTemplate(createTemplateIfNotExists(), RequestOptions.DEFAULT)
   }
@@ -89,6 +93,7 @@ abstract class AbstractElasticsearchKeyStoreHandler(
   }
 
   private fun flush() {
+//    var index = indexSelector.selectIndex()
     createIndexIfNotExists(property.index)
 
     val multiGetResponse = client.mget(multiGetRequestContainer.multiGetRequest, RequestOptions.DEFAULT)
@@ -130,15 +135,14 @@ abstract class AbstractElasticsearchKeyStoreHandler(
 
   @PreDestroy
   fun shutdown() {
-    scheduler.shutdown()
+    keyStoreScheduler.shutdown()
+    keyRotatorScheduler.shutdown()
     logger.info("Sleeping for 10 seconds to allow leftovers to be written")
     try {
       Thread.sleep(10000)
     } catch (ignored: InterruptedException) {
-    }
 
-    logger.info("Closing ES client")
-    client.close()
+    }
   }
 
   abstract fun mapToIndexRequests(metric: GrapheneMetric?) : List<IndexRequest>
