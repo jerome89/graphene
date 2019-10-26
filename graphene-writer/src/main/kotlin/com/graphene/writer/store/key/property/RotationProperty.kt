1e13ba46af9c0f2f193c4802cd09a0b0d8329728
dark <ekdxhrl0096@gmail.com>
2019-10-27 02:11:04 +0900

    feat: add rotation strategy about metric key

--- graphene-writer/src/main/kotlin/com/graphene/writer/store/key/property/RotationProperty.kt
+++ graphene-writer/src/main/kotlin/com/graphene/writer/store/key/property/RotationProperty.kt
@@ -3,9 +3,10 @@ package com.graphene.writer.store.key.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "graphene.writer.store.key.rotation")
-class RotationProperty {
-  var strategy: String = DEFAULT_STRATEGY
+data class RotationProperty(
+  var strategy: String = DEFAULT_STRATEGY,
   var period: String = PERIOD
+) {
 
   companion object {
     const val DEFAULT_STRATEGY = "timeBasedRotation"
--- graphene-writer/src/main/kotlin/com/graphene/writer/store/key/property/RotationStrategy.kt
+++ graphene-writer/src/main/kotlin/com/graphene/writer/store/key/property/RotationStrategy.kt
@@ -1,6 +1,8 @@
 package com.graphene.writer.store.key.property
 
-import java.time.LocalDateTime
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.threeten.extra.YearWeek
 import java.time.format.DateTimeFormatter
 
 interface RotationStrategy {
@@ -10,20 +12,34 @@ interface RotationStrategy {
   companion object {
     fun of(rotationProperty: RotationProperty): RotationStrategy {
       return when (rotationProperty.strategy) {
-        "timeBasedRotation" -> TimeBasedRotationStrategy(rotationProperty)
+        TIME_BASED_ROTATION -> TimeBasedRotationStrategy(rotationProperty)
         else -> TimeBasedRotationStrategy(rotationProperty)
       }
     }
+
+    private const val TIME_BASED_ROTATION = "timeBasedRotation"
   }
 }
 
 class TimeBasedRotationStrategy(
-  val rotationProperty: RotationProperty
+  rotationProperty: RotationProperty
 ) : RotationStrategy {
 
+  private var timeUnit = rotationProperty.period.toCharArray()[rotationProperty.period.lastIndex]
+  private var timePattern = DateTimeFormat.forPattern(DATE_FORMAT)
+
   override fun getDate(): String {
-    val currentTime = LocalDateTime.now()
-    return currentTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+    val dateTime = DateTime()
+
+    return when(timeUnit) {
+      DAY -> timePattern.print(dateTime)
+      else -> YearWeek.parse(timePattern.print(dateTime), DateTimeFormatter.ofPattern(DATE_FORMAT)).toString()
+    }
+  }
+
+  companion object {
+    const val DATE_FORMAT = "yyyyMMdd"
+    const val DAY = 'd'
   }
 
 }
