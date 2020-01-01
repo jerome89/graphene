109dd71c8f5692a30f2c9f7a9c2c9ec7ac2fbd8d
dark <ekdxhrl0096@gmail.com>
2020-01-01 22:56:29 +0900

    fix: fix wildcard import statement

--- graphene-writer/src/test/kotlin/com/graphene/writer/error/ErrorsTest.kt
+++ graphene-writer/src/test/kotlin/com/graphene/writer/error/ErrorsTest.kt
@@ -1,7 +1,7 @@
 package com.graphene.writer.error
 
 import com.graphene.writer.error.exception.UnknownGrapheneWriterException
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class ErrorsTest {
