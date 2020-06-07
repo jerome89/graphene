c24e8218e23bb6f4a68005493482d3317520eb89
dark <ekdxhrl0096@gmail.com>
2020-06-07 16:32:42 +0900

    feat: add graphene drop mechanism

--- graphene-writer/src/main/kotlin/com/graphene/writer/processor/GrapheneProcessor.kt
+++ graphene-writer/src/main/kotlin/com/graphene/writer/processor/GrapheneProcessor.kt
@@ -5,11 +5,16 @@ import com.graphene.writer.event.GrapheneDataStoreEvent
 import com.graphene.writer.input.GrapheneMetric
 import com.graphene.writer.store.StoreHandler
 import com.graphene.writer.store.StoreHandlerFactory
-import javax.annotation.PostConstruct
-import javax.annotation.PreDestroy
+import org.apache.logging.log4j.LogManager
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
 
 @Component
 class GrapheneProcessor(
@@ -18,17 +23,31 @@ class GrapheneProcessor(
   val storeHandlerFactory: StoreHandlerFactory
 ) {
 
+  val log = LogManager.getLogger(javaClass)
   var storeHandlers: MutableList<StoreHandler> = mutableListOf()
+  var dropCount: AtomicLong = AtomicLong(0)
+  lateinit var scheduler: ScheduledExecutorService
 
   @PostConstruct
   fun init() {
     storeHandlers.addAll(storeHandlerFactory.keyStoreHandlers())
     storeHandlers.addAll(storeHandlerFactory.dataStoreHandlers())
+    scheduler = Executors.newSingleThreadScheduledExecutor()
+    scheduler.scheduleWithFixedDelay({
+      log.info("Drop count : $dropCount")
+    }, 10, 10, TimeUnit.SECONDS)
   }
 
   @Async("grapheneProcessorExecutor")
   fun process(grapheneMetric: GrapheneMetric) {
     if (blacklistService.isBlackListed(grapheneMetric)) {
+      return
+    }
+
+    val metricKeyBytes = grapheneMetric.id.toByteArray()
+    if (METRIC_ID_LIMIT < metricKeyBytes.size) {
+      dropCount.incrementAndGet()
+      log.debug("Graphene has a metric key limit of 512 bytes. But it is ${metricKeyBytes.size} bytes. Drop the graphene metric : $grapheneMetric.")
       return
     }
 
@@ -44,5 +63,10 @@ class GrapheneProcessor(
     for (storeHandler in storeHandlers) {
       storeHandler.close()
     }
+    scheduler.shutdown()
+  }
+
+  companion object {
+    const val METRIC_ID_LIMIT = 512
   }
 }
